94fe2d0c4d18dee5724df2cef56f6570c9856156
Brian Ragazzi <brian@zimage.com>
2024-10-23 13:39:47 -0700

    Separate display from extraction of cluster info

--- distributed-rag/cluster.py
+++ distributed-rag/cluster.py
@@ -2,17 +2,32 @@ import chromadb
 
 remote_nodes = []
 
-def display_cluster_info():
+def get_cluster_info():
   clients = []
   for host, port in remote_nodes:
     client = chromadb.HttpClient(host=host, port=port)
     clients.append(client)
 
+  cluster = {'servers':[]}
   for client in clients:
     settings = client.get_settings()
-    print('Server %s:%d' % (settings['chroma_server_host'], settings['chroma_server_http_port']))
+    server = {
+      'host':settings['chroma_server_host'],
+      'port':settings['chroma_server_http_port'],
+      'collections': {}
+    }
     for collection in client.list_collections():
-      print('- %s: %d' % (collection.name, collection.count()))
+      server['collections'][collection.name] = collection.count()
+    cluster['servers'].append(server)
+
+  return cluster
+
+def display_cluster_info():
+  cluster = get_cluster_info()
+  for server in cluster['servers']:
+    print('Server %s:%d' % (server['host'], server['port']))
+    for name, count in server['collections'].items():
+      print('- %s: %d' % (name, count))
 
 def init():
   with open('nodes.txt') as fh:
