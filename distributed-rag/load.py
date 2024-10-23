6ddc6f8d74add7106b60c7165dc3b080072b16d4
Brian Ragazzi <brian@zimage.com>
2024-10-23 10:39:22 -0700

    Add article extraction from zim files to loader

--- distributed-rag/load.py
+++ distributed-rag/load.py
@@ -2,16 +2,23 @@ import chromadb
 from langchain_community.document_loaders import WikipediaLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter, SpacyTextSplitter, NLTKTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
+from libzim.reader import Archive
+import json
 import os
+import sys
 from uuid import uuid4
 
 
 remote_nodes = []
 
 
-def save_chunk(chunk, id):
-  with open(os.path.join('chunks', id), 'w') as fh:
-    fh.write(chunk)
+def save_chunk(chunk, id, metadata, save_content=True):
+  if save_content:
+    with open(os.path.join('chunks', id), 'w') as fh:
+      fh.write(chunk)
+  if metadata:
+    with open(os.path.join('chunks', '%s.json' % id), 'w') as fh:
+      json.dump(metadata, fh)
 
 def get_embedding_model():
   model_name = "sentence-transformers/all-mpnet-base-v2"
@@ -36,7 +43,35 @@ def wikipedia_embeddings():
     for chunk in chunks:
       embedding = embedding_model.embed_query(chunk)
       id = uuid4().hex
-      save_chunk(chunk, id)
+      save_chunk(chunk, id, None)
+      yield (embedding, id)
+
+def zim_file_entries(zim_file):
+  zim = Archive(zim_file)
+  entry_count = zim.all_entry_count
+  unique_paths = []
+  for i in range(entry_count):
+    entry = zim._get_entry_by_id(i)
+    path = entry.get_item().path
+    if path.startswith('A') and path not in unique_paths:
+      unique_paths = path
+      yield entry
+
+def zim_file_embeddings(zim_file):
+  text_splitter = SpacyTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+  embedding_model = get_embedding_model()
+
+  for entry in zim_file_entries(zim_file):
+    doc_metadata = {'file':zim_file, 'path':entry.path}
+    content = entry.get_item().content.tobytes().decode()
+    chunks = text_splitter.split_text(content)
+    for i, chunk in enumerate(chunks):
+      embedding = embedding_model.embed_query(chunk)
+      id = uuid4().hex
+      metadata = {'chunk':i, 'id':id}
+      metadata.update(doc_metadata)
+      save_chunk(chunk, id, metadata)
       yield (embedding, id)
 
 def load_data(collections, get_embeddings, batch_size=1):
@@ -60,7 +95,7 @@ def load_data(collections, get_embeddings, batch_size=1):
   for i in range(shard_count):
     collections[i].add(embeddings=embeddings[i], ids=ids[i])
 
-def build_database():
+def build_database(zim_file):
   clients = []
   for host, port in remote_nodes:
     client = chromadb.HttpClient(host=host, port=port)
@@ -74,7 +109,12 @@ def build_database():
       pass
   collections = [c.create_collection(collection_name) for c in clients]
 
-  load_data(collections, wikipedia_embeddings, batch_size=10)
+  if zim_file:
+    def get_embeddings():
+      return zim_file_embeddings(zim_file)
+    load_data(collections, get_embeddings, batch_size=100)
+  else:
+    load_data(collections, wikipedia_embeddings, batch_size=10)
 
 def init():
   with open('nodes.txt') as fh:
@@ -82,10 +122,13 @@ def init():
       host, port = line.strip().split(':')
       remote_nodes.append((host, port))
 
-def main():
+def main(zim_file=None):
   init()
-  build_database()
+  build_database(zim_file)
 
 if __name__ == '__main__':
-  main()
+  if len(sys.argv) == 2:
+    main(sys.argv[1])
+  else:
+    main()
 
